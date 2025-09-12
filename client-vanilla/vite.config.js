01a44a87f7ffc98ce3af6e4317fbd0cec8dc7cb6
souravdhar <bontaramsonta@gmail.com>
2025-09-12 17:47:22 +0530

    moved client

---
+++ client-vanilla/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js",
+          dest: "./",
+        },
+        {
+          src: "node_modules/@ricky0123/vad-web/dist/silero_vad_v5.onnx",
+          dest: "./",
+        },
+        {
+          src: "node_modules/onnxruntime-web/dist/*.wasm",
+          dest: "./",
+        },
+        {
+          src: "node_modules/onnxruntime-web/dist/*.mjs",
+          dest: "./",
+        },
+      ],
+    }),
+  ],
+  server: {
+    port: 5173,
+    host: true,
+  },
+});
